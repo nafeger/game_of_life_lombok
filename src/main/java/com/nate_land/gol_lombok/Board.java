047299eb1a9d77ee473d15a004a915172f1f2cd4
Nathan Feger <nfeger@spredfast.com>
2014-08-23 19:34:28 -0500

    Possibly working system

--- src/main/java/com/nate_land/gol_lombok/Board.java
+++ src/main/java/com/nate_land/gol_lombok/Board.java
@@ -2,31 +2,62 @@ package com.nate_land.gol_lombok;
 
 import java.util.List;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+
+import lombok.Value;
 
 /**
  *
  */
 public class Board {
 	private final int size;
+	private final int maxIterations;
+	private final int currentIteration;
 	private final List<List<Character>> gameBoard;
 	
-	public Board(int size) {
-		this.size = size;
-		ImmutableList.Builder<List<Character>> builder = new ImmutableList.Builder<>();
-		for(int i = 0; i < size; i++) {
-			List<Character> chars = Lists.newArrayListWithExpectedSize(size);
-			IntStream.range(0,size).forEach(t -> chars.add(' '));
-			builder.add(chars);
-		}
+	public Board(Board board) {
+		this.gameBoard = board.gameBoard;
+		this.size = board.size;
+		this.currentIteration = board.currentIteration+1;
+		this.maxIterations = board.maxIterations;
+		nodes().forEach(n -> {
+			if (!n.isAlive()) {
+				if (n.neighborCount() == 3) {
+					setNode(n, true);
+				}
+			} else {
+				if (n.neighborCount() < 2) {
+					setNode(n, false);
+				} else if (n.neighborCount() == 2 || n.neighborCount() == 3) {
+					setNode(n, true);
+				} else if (n.neighborCount() > 3) {
+					setNode(n, false);
+				}
+			}
+		});
+	}
+
+	private void setNode(Node n, boolean alive) {
+		this.gameBoard.get(n.getRow()).set(n.getColumn(), alive ? 'x' : ' ');
+	}
+
+	Stream<Node> nodes() {
+		return IntStream.range(0, size*size-1).mapToObj(i -> Node.of(this, i / this.size, i % this.size));
+	}
+
+	public Board(List<String> readBoard, int maxIterations, int currentIteration) {
+		this.size = Math.max(readBoard.size(), readBoard.parallelStream().map(l -> l.length()).max(Integer::compare).get());
+		this.maxIterations = maxIterations;
+		this.currentIteration = currentIteration;
+
+		ImmutableList.Builder<List<Character>> builder = buildBoard(readBoard);
 		this.gameBoard = builder.build();
 	}
 
-	public Board(List<String> readBoard) {
-		this.size = Math.max(readBoard.size(), readBoard.parallelStream().map(l -> l.length()).max(Integer::compare).get());
-		
+	private ImmutableList.Builder<List<Character>> buildBoard(List<String> readBoard) {
 		ImmutableList.Builder<List<Character>> builder = new ImmutableList.Builder<>();
 		for(int i = 0; i < size; i++) {
 			List<Character> chars = Lists.newArrayListWithExpectedSize(size);
@@ -36,7 +67,8 @@ public class Board {
 		
 			builder.add(chars);
 		}
-		this.gameBoard = builder.build();
+		List<List<Character>> board = builder.build();
+		return builder;
 	}
 
 	@Override
@@ -49,4 +81,59 @@ public class Board {
 		return sb.toString();
 	}
 
+	public boolean hasNext() {
+		return this.maxIterations > this.currentIteration;
+	}
+
+	@Value(staticConstructor = "of")
+	public static class Node {
+		private final Board board;
+		private final int row;
+		private final int column;
+
+		public boolean isAlive() {
+			return board.gameBoard.get(row).get(column) == 'x';
+		}
+
+		public long neighborCount() {
+			return neighbors().filter(n -> n.isAlive()).count();
+		}
+
+		/**
+		 * position
+		 * 012 
+		 * 3x4 
+		 * 567
+		 * @return
+		 */
+		public Stream<Node> neighbors() {
+			List<Node> rv = Lists.newLinkedList();
+			if (row - 1 >= 0 && column - 1 >= 0) { // top left
+				rv.add(of(this.board, row - 1, column - 1));
+			}
+			if (row - 1 >= 0) { // top
+				rv.add(of(this.board, row - 1, column ));
+			}
+			if (row - 1 >- 0 && column + 1 < board.size) { //top right
+				rv.add(of(this.board, row - 1, column + 1));
+			}
+			if (column - 1 >= 0) { // left
+				rv.add(of(this.board, row , column - 1));
+			}
+			if (column + 1 < board.size) { // right
+				rv.add(of(this.board, row , column + 1));
+			}
+			if (row + 1 < board.size && column - 1 >= 0) { // bottom left
+				rv.add(of(this.board, row + 1, column - 1));
+			}
+			if (row + 1 < board.size ) { // bottom 
+				rv.add(of(this.board, row + 1, column ));
+			}
+			if (row + 1 < board.size && column + 1 < board.size) { // bottom right
+				rv.add(of(this.board, row + 1, column + 1));
+			}
+			
+			return rv.stream();
+		}
+	}
 }
--- src/main/java/com/nate_land/gol_lombok/Driver.java
+++ src/main/java/com/nate_land/gol_lombok/Driver.java
@@ -5,7 +5,16 @@ import java.nio.file.Files;
 
 public class Driver {
 	public static void main(String... args) throws Exception {
-		Board b = new Board(Files.readAllLines(FileSystems.getDefault().getPath("examples/example.board")));
-		System.out.println(b);
+		Board b = new Board(Files.readAllLines(FileSystems.getDefault().getPath("examples/example.board")), 10, 0);
+		while(b.hasNext()) {
+			System.out.println("top");
+			System.out.println(b);
+			b = new Board(b);
+			Thread.sleep(15 * 100);
+			Runtime.getRuntime().exec("clear");
+//			System.out.print("\u001b[2J");
+//			System.out.print("\b");
+//			System.out.flush();
+		}
 	}
 }
